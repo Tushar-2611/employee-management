1679bae06814aba6566b6abcf0fdcd52bac9cc09
Tushar-2611 <tushar.kadam.n@gmail.com>
2026-05-31 01:19:42 +0530

    created DTOs for request and resposne

---
+++ src/main/java/com/tushar/employee_management/dto/EmployeeRequestDTO.java
@@ -0,0 +1,23 @@
+package com.tushar.employee_management.dto;
+
+public class EmployeeRequestDTO {
+
+    private String name;
+    private String department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
--- src/main/java/com/tushar/employee_management/employee/EmployeeController.java
+++ src/main/java/com/tushar/employee_management/employee/EmployeeController.java
@@ -2,7 +2,6 @@ package com.tushar.employee_management.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 public class EmployeeController {
