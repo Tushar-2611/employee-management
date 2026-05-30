b41b723b30c62a0e2a76df9aee6c0dabce284eaf
Tushar-2611 <tushar.kadam.n@gmail.com>
2026-05-30 16:46:47 +0530

    Created a Service layer to handle business logic

---
+++ src/main/java/com/tushar/employee_management/employee/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.tushar.employee_management.employee;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(
+                        "Employee not found"));
+    }
+}
